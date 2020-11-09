dff16f25026ac6ada135703d12b74e90f6a4429b
nishi <sonnabakananz@gmail.com>
2020-11-09 14:04:03 +0900

    first_commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
-  validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
-  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
+  # format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   has_secure_password
   validates :password, presence: true, length: { minimum: 7 }
 end
